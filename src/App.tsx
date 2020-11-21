59464dc61d69a9b9fd8d679d9f19ad3cf4bd17da
ianertson <sebbekarlsson97@gmail.com>
2020-11-21 19:35:36 +0100

    update example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React, { FC, useCallback } from "react";
+import React, { FC, useCallback, useMemo } from "react";
 import { dispatcher } from "./store";
 import "./App.css";
 import { IAppProps } from "./types";
 import { CONTAINER_CLICKER_AMOUNT } from "./store/containers";
 import { withState0 } from "./store/with";
+import prettyFormat from "pretty-format";
 
 const AppComponent: FC<IAppProps> = ({ amount }): JSX.Element => {
   const handleClick = useCallback(() => {
     dispatcher.emit(CONTAINER_CLICKER_AMOUNT, { amount: 1 });
   }, [dispatcher]);
+
+  const raw = useMemo(
+    () =>
+      prettyFormat({
+        state: dispatcher.state,
+      }),
+    [dispatcher.state, dispatcher.listeners]
+  );
+
   return (
     <div className="wrapper">
       <section>
@@ -17,6 +27,16 @@ const AppComponent: FC<IAppProps> = ({ amount }): JSX.Element => {
       <section>
         <p>You have pressed me {amount} times.</p>
       </section>
+      {raw && (
+        <section>
+          <i
+            style={{ marginTop: "1rem", display: "block", fontSize: "0.8rem" }}
+          >
+            What it looks like inside
+          </i>
+          <pre className="textarea">{raw}</pre>
+        </section>
+      )}
     </div>
   );
 };
